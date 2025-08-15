4bedb63bd965a17c7939e4e1dec9c5dfbdd9cb67
Matt Joiner <anacrolix@gmail.com>
2025-08-15 11:17:57 +1000

    Declare lsan function in case header is missing

--- lsan.go
+++ lsan.go
@@ -4,13 +4,18 @@
 // ASAN_OPTIONS=detect_leaks=1,halt_on_error=0. Since Go 1.18 you might also need
 // CGO_CPPFLAGS_ALLOW='.*' and CGO_LDFLAGS_ALLOW='.*' for the -fsanitize-recover. On MacOS you may
 // need to specify a custom clang for address sanitizor support
-// (https://stackoverflow.com/a/55778432/149482). There's an interface defined at
-// https://chromium.googlesource.com/chromiumos/third_party/compiler-rt/+/release_34/include/sanitizer/lsan_interface.h
-// . I can't work out what the proper include is.
+// (https://stackoverflow.com/a/55778432/149482).
 package lsan
 
 // #cgo CPPFLAGS: -O0 -g -fsanitize=address -fsanitize-recover=address
 // #cgo LDFLAGS: -fsanitize=address -g -fsanitize-recover=address
+//
+// /*
+// There's an interface defined at
+// https://chromium.googlesource.com/chromiumos/third_party/compiler-rt/+/release_34/include/sanitizer/lsan_interface.h.
+// I can't work out what the proper include is.
+// */
+// void __lsan_do_leak_check(void);
 import "C"
 
 func DoLeakCheckIfLsanEnabled() {
